583ce63a22ae78627afcd306498a55c78bbb7471
Evgenia Gruchenkova <gruchenkovaevgenia@yandex.by>
2021-03-15 21:32:02 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports =
+function check(str, bracketsConfig) {
+  let stack = [];
+  Object.defineProperty(stack, 'lastItem', {
+    enumerable: false,
+    get() {
+      return this[this.length - 1];
+    }
+  });
+ let openBrackets= [],
+    closeBrackets= [];
+    if (str.length %2 !=0){
+    return false
+    }
+  for (let i = 0; i < bracketsConfig.length; i++) {
+   openBrackets.push(bracketsConfig[i][0])
+   closeBrackets.push(bracketsConfig[i][1])
+  }
+  for (let k = 0; k < str.length; k++) {
+    if (openBrackets.includes(str[k])&&closeBrackets.includes(str[k])&& stack.lastItem === str[k]) {
+      stack.pop()
+    } else if (openBrackets.includes(str[k])&& closeBrackets.includes(str[k])&& stack.lastItem != str[k]){
+      stack.push(str[k])
+    }else if (openBrackets.includes(str[k])){
+    stack.push(str[k])
+    }else if (closeBrackets.includes(str[k])){
+    for (let d=0;d<bracketsConfig.length; d++){
+    if (bracketsConfig[d][1]===str[k]&&bracketsConfig[d][0]===stack.lastItem){
+    stack.pop()
+    }
+    }
+    }
+  }
+  if (stack.length === 0){
+  return true
+  }else return false
 }
